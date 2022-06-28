e14c805f82a87a0ccb4db3768cee081640cf0d7d
Rico Schrage <rico.schrage@uni-oldenburg.de>
2022-06-28 18:37:55 +0200

    Add settings page, add muicss.

--- mango_ui/interactive.py
+++ mango_ui/interactive.py
@@ -1,13 +1,32 @@
 
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import dash_daq as daq
 
 import dash
 
-def show_result_interactive(debug=False):
+import mango_ui.pages.settings as uis
 
-    app = dash.Dash(__name__, suppress_callback_exceptions=True)
+def show_result_interactive(debug=False):
+    external_scripts = [
+        {
+            'src': '//cdn.muicss.com/mui-0.10.3/js/mui.min.js',
+        }
+    ]
+
+    # external CSS stylesheets
+    external_stylesheets = [
+        {
+            'href': '//cdn.muicss.com/mui-0.10.3/css/mui.min.css',
+            'rel': 'stylesheet',
+            'type': 'text/css'
+        }
+    ]
+
+    app = dash.Dash(__name__, suppress_callback_exceptions=True,
+        external_scripts=external_scripts,
+        external_stylesheets=external_stylesheets)
     app.title = 'mango UI'
 
     # graph + detail time series
@@ -16,12 +35,22 @@ def show_result_interactive(debug=False):
             html.Div(["mango UI"], id='head-title'), 
             html.Div([
                 dcc.Link("View", href='/view'), 
-                dcc.Link("Setup", href='/setup')
+                dcc.Link("Settings", href='/settings')
             ], className='nav')
         ], className='head'), 
         dcc.Location(id="url", refresh=False),
         html.Div(id='page-content',  className='content'), 
+        html.Div(id='settings-container',  className='content'), 
     ], id='page')
+
+    @app.callback(Output("settings-container", "children"), 
+                  Output("settings-container", "style"), 
+                  [Input("url", "pathname")])  
+    def display_page(path):
+        if path == '/view':
+            return [], {"display": "none"}
+        elif path == '/settings':
+            return uis.create_setup_layout(), {"display": "flex"}
 
     app.run_server(debug=debug)
 
